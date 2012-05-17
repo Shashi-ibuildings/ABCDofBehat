97a4b9800d5d6c275fd79049869af060390dd4a8
Shashikant jagtap <sjagtap@ibuildings.com>
2012-05-17 22:15:00 +0100

    Added SubContext

--- features/bootstrap/SubContext.php
+++ features/bootstrap/SubContext.php
@@ -1,15 +1,19 @@
 <?php
 
-class MyFeatureContext extends Behat\MinkExtension\Context\MinkContext
+class SubContext extends Behat\Behat\Context\BehatContext
 {
+    public function __construct()
+    {
+        $this->useContext('mink', new Behat\MinkExtension\Context\MinkContext);
+    }
        /**
      * @When /^I search for "([^"]*)"$/
           */
     
     public function iSearchFor($searchKeyword)
     {
-        $this->fillField("s", $searchKeyword);
-        $this->pressButton("Search");
+        $this->getSubcontext('mink')->fillField("s", $searchKeyword);
+        $this->getSubcontext('mink')->pressButton("Search");
     }
 
 
